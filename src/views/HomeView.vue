c73e04fefcec93447959a402a03811e77754b5c6
Naru11 <2925315190@qq.com>
2023-10-24 10:51:38 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,16 @@
 <template>
   <ContentBase>首页</ContentBase>
-  
 </template>
 
 <script>
 // @ is an alias to /src
-import ContentBase from '@/components/ContentBase.vue';
-import { ref } from 'vue';
+import ContentBase from '@/components/ContentBase.vue'
 export default {
   name: 'HomeView',
   components: {
-    ContentBase,
-  },
-  setup() {
-    const a = ref(true)
-    return {
-      a,
-    
-    }
+    ContentBase
   }
 }
 </script>
 <style scoped>
-.night {
-  background-color: black;
-}
 </style>
